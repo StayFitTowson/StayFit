3bf9dc1fd42a2f4b374622d571af22f9ce8371ad
Oleg Lebediev <lebedyevoleg@gmail.com>
2017-11-09 20:57:25 -0500

    changing stuff

--- db/migrate/20171014004454_remove_blocked_from_relationships.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBlockedFromRelationships < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :relationships, :blocked, :boolean
-  end
-end
--- db/migrate/20171103003914_add_completed_on_to_achievements.rb
+++
@@ -1,5 +0,0 @@
-class AddCompletedOnToAchievements < ActiveRecord::Migration[5.1]
-  def change
-    add_column :achievements, :completed_on, :date
-  end
-end
